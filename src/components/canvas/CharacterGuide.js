a08d6663467d2d0684b16316eb929ac3fe1bb65e
Huynh Duc Dung <dunghd.it@gmail.com>
2026-03-21 00:40:21 +0800

    refactor(canvas): remove unused Canvas and CharacterGuide components

--- src/components/canvas/CharacterGuide.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export const CharacterGuide = ({ template }) => {
-    return (_jsx("div", { className: "character-guide", children: _jsx("span", { children: template.character }) }));
-};
